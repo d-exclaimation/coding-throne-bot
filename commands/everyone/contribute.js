de57776a44ee26c80eb0b5b5378f78f0d28dc9aa
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-17 18:17:31 +0100

    🐜 added `contribute

---
+++ commands/everyone/contribute.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "contribute",
+    description: "shows how to contribute",
+    category: "general",
+    execute: (message, args) => {
+        message.channel.send("this is how")
+    }
+}
