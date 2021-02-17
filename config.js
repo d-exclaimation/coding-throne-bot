64dcd6d328c1fada3528f134fc16a230d9016cef
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-17 08:10:29 +0100

    ✏️ Added config, moved prefix there

---
+++ config.js
@@ -0,0 +1,10 @@
+function configGenerator(...params) {
+  /* Destructure params as you see fit */
+  const {} = params;
+  
+  return {
+    prefix: "`",
+  };
+}
+
+module.exports = configGenerator();
